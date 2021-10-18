91358d42e1d311f0ddf45aff88d4a67ef45a77fd
nholmes7 <76012924+nholmes7@users.noreply.github.com>
2021-10-17 23:41:19 -0400

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import time
-start_time = time.time()
-with open('data_log','a') as file:
-    for i in range(10000):
-        file.writelines('This is a test to see how long this takes.')
-
-test_duration = time.time()-start_time
-print('It took ' + str(test_duration) + ' s to write 1000 lines.')
